7cbdd933d735448fae8e968342e7fb8299069d37
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-06-28 16:25:16 +0200

    feat: api request done correctly

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import axios from "axios";
+
+	export default{
+		data(){
+			return{
+				posts: [],
+			}
+		},
+		created() {
+			axios.get('http://127.0.0.1:8000/api/projects').then((resp)=> {
+				this.posts = resp.data.results;
+        console.log(this.posts);
+			});
+		}
+	}
+</script>
+
+<template>
+  <h1>Home</h1>
+</template>
+
+<style></style>
