116230c86d451a51ad86261ec5bd9353349f7805
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-06-28 17:07:14 +0200

    feat: created ProjectCard component and displayed information

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <script>
-  import axios from "axios";
+import axios from "axios";
+import ProjectCard from './components/ProjectCard.vue';
 
-	export default{
-		data(){
-			return{
-				posts: [],
-			}
-		},
-		created() {
-			axios.get('http://127.0.0.1:8000/api/projects').then((resp)=> {
-				this.posts = resp.data.results;
-        console.log(this.posts);
-			});
-		}
-	}
+export default {
+  
+  data() {
+    return {
+      projects: [],
+      baseImageUrl: 'http://127.0.0.1:8000/storage'
+    };
+  },
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+      this.projects = resp.data.results.data;
+      console.log(this.projects);
+    });
+  },
+  components: {
+    ProjectCard
+  }
+};
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div class="container mt-3">
+    <div class="row g-4 row-cols-lg-4">
+      <div class="col" v-for="(project,index) in projects" :key="index">
+        <ProjectCard :project="project" />
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    project: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card h-100 w-100" style="width: 18rem">
+    <img src="https://placehold.co/600x400" class="card-img-top" alt="..." />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <p class="card-text">
+        {{ project.description }}
+      </p>
+      <div v-if="project.technologies.length > 0" class="d-flex flex-column">
+
+          <span>Technologies:</span>
+          <div class="badge w-25" v-for="(technology,index) in project.technologies" :key="index" :style="{backgroundColor: technology.color}">{{ technology.name }}</div>
+      </div>
+      <span>Types:</span>
+      <div class="badge" :style="{backgroundColor: project.type.color}">{{ project.type.name }}</div>
+      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+  </div>
+</template>
+
+<style></style>
